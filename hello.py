18b6928310f7f0d9fa640b481ae260e5d4643b88
vinay nair <vinaynair21@gmail.com>
2021-09-22 14:06:38 +0530

    latest

--- hello.py
+++ hello.py
@@ -8,4 +8,4 @@ def index():
    return render_template('index.html')
 
 if __name__ == '__main__': 
-   app.run(port=5000, debug=True) # application will start listening for web request on port 5000
+   app.run(port=5001, debug=True) # application will start listening for web request on port 5000
