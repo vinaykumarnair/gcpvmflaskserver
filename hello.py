48aded01e5db4857d185e7583b53d757dac61f02
vinay nair <vinaynair21@gmail.com>
2021-09-22 12:42:49 +0530

    new changes

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 # File name: hello_webapp.py 
-from flask import Flask # include the flask library 
+from flask import Flask, render_template # include the flask library 
 
 app = Flask(__name__) 
 
 @app.route("/") 
 def index(): 
-   return "Hello, World!" 
+   return render_template('index.html')
 
 if __name__ == '__main__': 
    app.run(port=5000, debug=True) # application will start listening for web request on port 5000
